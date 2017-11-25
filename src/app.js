de7757f6b42ff6d1235346a9ac77aa074526a9e8
CD Cabrera <cabrera.code@gmail.com>
2017-11-25 03:30:56 -0500

    feat(app):test semantic

--- src/app.js
+++ src/app.js
@@ -25,7 +25,9 @@ class App {
 
         if (this.options.debug) {
 
+            console.log('alert1', this.options);
             console.log('alert2', this.options);
+            console.log('alert3', this.options);
         }
     }
 }
